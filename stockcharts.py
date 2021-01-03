79de775267bfc30cb4e9a7b97f16e91c705d4aae
iDevBrandon <hsy0602@outlook.com>
2021-01-04 04:00:25 +0900

    need to work on to get load several tickers

--- stockcharts.py
+++ stockcharts.py
@@ -6,15 +6,21 @@ import urllib.request
 driver = webdriver.Chrome()
 driver.get("https://stockcharts.com/")
 elem = driver.find_element_by_id("nav-chartSearch-input")
+
 elem.send_keys("spy")
+# elem.send_keys("bnd")
+
 elem.send_keys(Keys.RETURN)
 time.sleep(3)
 imgUrl = driver.find_element_by_css_selector(".chartimg").get_attribute("src")
 
+count = 1
+
 opener=urllib.request.build_opener()
 opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
 urllib.request.install_opener(opener)
-urllib.request.urlretrieve(imgUrl, "test.jpg")
+urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+count = count + 1
 
 # assert "Python" in driver.title
 # elem = driver.find_element_by_name("q")
