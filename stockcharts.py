03ebadefb237a1c336f203edf6217d34646e34dc
iDevBrandon <hsy0602@outlook.com>
2021-01-04 03:13:06 +0900

    enter "spy" on stockcharts.com

--- stockcharts.py
+++ stockcharts.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
+driver = webdriver.Chrome()
+driver.get("https://stockcharts.com/")
+elem = driver.find_element_by_id("nav-chartSearch-input")
+elem.send_keys("spy")
 elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close() 
+
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close() 
