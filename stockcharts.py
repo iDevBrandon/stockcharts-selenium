922cbe83755b7ff3b2c011d1a012b8e67a930d68
iDevBrandon <hsy0602@outlook.com>
2021-01-04 17:04:24 +0900

    code works

--- stockcharts.py
+++ stockcharts.py
@@ -2,30 +2,32 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import urllib.request
- 
+
 driver = webdriver.Chrome()
 driver.get("https://stockcharts.com/")
-elem = driver.find_element_by_id("nav-chartSearch-input")
 
-elem.send_keys("spy")
-# elem.send_keys("bnd")
-
-elem.send_keys(Keys.RETURN)
-time.sleep(3)
-imgUrl = driver.find_element_by_css_selector(".chartimg").get_attribute("src")
-
+# ticker symbols array
+symbols = ['spy', 'aapl', 'msft', 'vxx', 'goog']
 count = 1
+for i in symbols:
 
-opener=urllib.request.build_opener()
-opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-urllib.request.install_opener(opener)
-urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
-count = count + 1
 
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close() 
+
+
+    driver.find_element_by_id("nav-chartSearch-input").send_keys(i+Keys.RETURN)
+
+    # elem.send_keys("spy")
+
+
+    
+    driver.implicitly_wait(2)
+    imgUrl = driver.find_element_by_css_selector(".chartimg").get_attribute("src")
+
+
+    opener = urllib.request.build_opener()
+    opener.addheaders = [
+        ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
+    count = count + 1
+    driver.back()
